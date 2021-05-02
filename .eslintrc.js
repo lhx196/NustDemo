f414c876ef714267c4998d175e664212816051d5
lhx196 <1012232293@qq.com>
2021-05-02 16:01:09 +0800

    脚手架搭建

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: [
+    "@babel/plugin-proposal-private-methods", { "loose": true }
+  ],
+  // add your custom rules here
+  rules: {},
+}
